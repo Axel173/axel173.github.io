2e600e1433a85851f2da51b8273cb9dd48b84976
Axel173 <alex310197@live.com>
2018-09-16 02:29:19 +0400

    -

--- js/init.js
+++ js/init.js
@@ -1,22 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     M.AutoInit();
 
-
-    /*elems23 = document.querySelectorAll('.autocomplete');
-    instances23 = M.Autocomplete.init(elems23, {
-        data: {
-            "Apple": null,
-            "Microsoft": null,
-            "Google": 'https://placehold.it/250x250'
-        }
-    });*/
-    /*var instance = M.Autocomplete.getInstance(document.querySelectorAll('.autocomplete')[0]);
-    instance.updateData({
-        "Apple": null,
-        "Microsoft": null,
-        "Google": 'https://placehold.it/250x250'
-    });*/
-
     M.Tabs.init('.tabs', {
 
     });
@@ -27,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     var timeoutID = 0;
-    console.log('первый timeoutID ' + timeoutID);
+
     var instance = M.Carousel.init(document.querySelectorAll('.carousel'), {
         dist: 0,
         padding: 0,
@@ -59,52 +43,11 @@ document.addEventListener('DOMContentLoaded', function () {
         timeoutID = setTimeout(autoplay, 4500);
     }
 
-
-
     var elems = document.querySelectorAll('.fixed-action-btn');
 
     var instances = M.FloatingActionButton.init(elems, {
         /*toolbarEnabled: true,*/
         hoverEnabled: false
     });
-
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems,
-        {
-            //edge: 'right'
-        });
-
-
-   /* var tap_target = document.querySelectorAll('.tap-target');
-    console.log(tap_target);
-
-    var instances = M.TapTarget.init(tap_target, {
-
-    });
-    //console.log(instances);
-
-
-    var instance = M.TapTarget.getInstance(tap_target[0]);
-    //console.log(instance);
-    /!*
-    instance.open();*!/
-
-    elems = document.querySelectorAll('.test');
-    var index, button;
-
-    for (index = 0; index < elems.length; index++) {
-        button = elems[index];
-        //button.addEventListener('click', clickHandler);
-        /!*button.addEventListener('dblclick', doubleClickHandler);*!/
-    }
-
-    function clickHandler(event) {
-        //console.log('клик');
-
-        instance.open();
-        //console.log(instance);
-        event.preventDefault();
-    }*/
-
 
 });
